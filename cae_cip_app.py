12e9ee76672397c8eb823314291f29d0baba9bd7
codexceed <sarthak.joshi94@yahoo.in>
2016-05-14 22:31:26 +0530

    New

--- cae_cip_app.py
+++ cae_cip_app.py
@@ -18,7 +18,8 @@ class Cipher(QDialog, cae_ui.Ui_cae):
         self.decrypt.clicked.connect(lambda: self.encrypt_text(-1))
 
 
-    cip_dic = {0:"a", 1:"b", 2:"c", 3:"d", 4:"e", 5:"f", 6:"g", 7:"h", 8:"i", 9:"j", 10:"k", 11:"l", 12:"m", 13:"n", 14:"o", 15:"p", 16:"q", 17:"r", 18:"s", 19:"t", 20:"u", 21:"v", 22:"w", 23:"x", 24:"y", 25:"z"}
+    cip_dic = {0:"a", 1:"b", 2:"c", 3:"d", 4:"e", 5:"f", 6:"g", 7:"h", 8:"i", 9:"j", 10:"k", 11:"l", 12:"m", 13:"n",
+               14:"o", 15:"p", 16:"q", 17:"r", 18:"s", 19:"t", 20:"u", 21:"v", 22:"w", 23:"x", 24:"y", 25:"z"}
 
 
     def encrypt_text(self, flag):
